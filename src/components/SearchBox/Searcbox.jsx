1cd881fe5627c770d248df1dcf5a07afc9e52242
Prosper Samuel <prospersamuel100@gmail.com>
2025-01-10 13:38:15 +0100

    'initial-comit'

---
+++ src/components/SearchBox/Searcbox.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../SearchBox/Searchbox.css'
+
+const Searcbox = () => {
+  return (
+    <div>
+        <input type="search"
+        placeholder='Search Tracking Id...'
+        />
+    </div>
+  )
+}
+
+export default Searcbox
