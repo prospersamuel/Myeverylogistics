d51b14383986c91108a7e80c3ee14a836e0a7d11
Prosper Samuel <prospersamuel100@gmail.com>
2025-01-10 15:03:02 +0100

    added changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,182 +1,125 @@
-import React from 'react'
+import React from "react";
+import { Link, NavLink } from "react-router-dom";
 
-const Header = () => {
+export default function Header() {
   return (
-
-
-<header className="bg-white">
-  <nav
-    className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
-    aria-label="Global"
-  >
-    <div className="flex lg:flex-1">
-      <a href="#" className="-m-1.5 p-1.5">
-        <span className="sr-only">Your Company</span>
-        <img
-          className="h-8 w-auto"
-          src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-          alt=""
-        />
-      </a>
-    </div>
-    <div className="flex lg:hidden">
-      <button
-        type="button"
-        className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg
-          className="size-6"
-          fill="none"
-          viewBox="0 0 24 24"
-          strokeWidth="1.5"
-          stroke="currentColor"
-          aria-hidden="true"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-          />
-        </svg>
-      </button>
-    </div>
-    <div className="hidden lg:flex lg:gap-x-12">
-      <div className="relative">
-        <button
-          type="button"
-          className="flex items-center gap-x-1 text-sm font-semibold text-gray-900"
-          aria-expanded="false"
-        >
-          Product
-          <svg
-            className="size-5 flex-none text-gray-400"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              fillRule="evenodd"
-              d="M5.22 8.22a.75.75 0 0 1 1.06 0L10 11.94l3.72-3.72a.75.75 0 1 1 1.06 1.06l-4.25 4.25a.75.75 0 0 1-1.06 0L5.22 9.28a.75.75 0 0 1 0-1.06Z"
-              clipRule="evenodd"
+    <header className="shadow sticky z-50 top-0">
+      <nav className="bg-white border-gray-200 px-4 lg:px-6 py-2.5">
+        <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
+          <Link to="/" className="flex items-center">
+            <img
+              src="https://myeverydeal.com/wp-content/uploads/2024/04/myeverydeal-website-logo-size-1-1-1536x415.webp"
+              className="mr-3 h-12"
+              alt="Logo"
             />
-          </svg>
-        </button>
-        {/* Flyout menu */}
-        <div className="absolute top-full -left-8 z-10 mt-3 w-screen max-w-md overflow-hidden rounded-3xl bg-white shadow-lg ring-1 ring-gray-900/5">
-          <div className="p-4">
-            {/* Menu items */}
-            {[
-              {
-                title: "Analytics",
-                description: "Get a better understanding of your traffic",
-                iconPath:
-                  "M10.5 6a7.5 7.5 0 1 0 7.5 7.5h-7.5V6Z M13.5 10.5H21A7.5 7.5 0 0 0 13.5 3v7.5Z",
-              },
-              // Add other items as objects here
-            ].map((item, idx) => (
-              <div
-                key={idx}
-                className="group relative flex items-center gap-x-6 rounded-lg p-4 text-sm hover:bg-gray-50"
-              >
-                <div className="flex size-11 flex-none items-center justify-center rounded-lg bg-gray-50 group-hover:bg-white">
-                  <svg
-                    className="size-6 text-gray-600 group-hover:text-indigo-600"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    strokeWidth="1.5"
-                    stroke="currentColor"
-                    aria-hidden="true"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      d={item.iconPath}
-                    />
-                  </svg>
-                </div>
-                <div className="flex-auto">
-                  <a href="#" className="block font-semibold text-gray-900">
-                    {item.title}
-                    <span className="absolute inset-0"></span>
-                  </a>
-                  <p className="mt-1 text-gray-600">{item.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-      <a href="#" className="text-sm font-semibold text-gray-900">
-        Features
-      </a>
-      <a href="#" className="text-sm font-semibold text-gray-900">
-        Marketplace
-      </a>
-      <a href="#" className="text-sm font-semibold text-gray-900">
-        Company
-      </a>
-    </div>
-    <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-      <a href="#" className="text-sm font-semibold text-gray-900">
-        Log in <span aria-hidden="true">&rarr;</span>
-      </a>
-    </div>
-  </nav>
-  {/* Mobile menu */}
-  <div className="lg:hidden" role="dialog" aria-modal="true">
-    <div className="fixed inset-0 z-10"></div>
-    <div className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
-      <div className="flex items-center justify-between">
-        <a href="#" className="-m-1.5 p-1.5">
-          <span className="sr-only">Your Company</span>
-          <img
-            className="h-8 w-auto"
-            src="https://tailwindui.com/plus/img/logos/mark.svg?color=indigo&shade=600"
-            alt=""
-          />
-        </a>
-        <button type="button" className="-m-2.5 rounded-md p-2.5 text-gray-700">
-          <span className="sr-only">Close menu</span>
-          <svg
-            className="size-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth="1.5"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M6 18 18 6M6 6l12 12"
-            />
-          </svg>
-        </button>
-      </div>
-      <div className="mt-6 flow-root">
-        <div className="-my-6 divide-y divide-gray-500/10">
-          <div className="space-y-2 py-6">
-            {/* Mobile menu items */}
-            <a
-              href="#"
-              className="block rounded-lg py-2 pr-3 text-sm font-semibold text-gray-900 hover:bg-gray-50"
+          </Link>
+          <div className="flex items-center lg:order-2">
+            <Link
+              to="#"
+              className="text-gray-800 hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none"
             >
-              Features
-            </a>
-            {/* Add more items here */}
+              Log in
+            </Link>
+            <Link
+              to="#"
+              className="text-white bg-[#EF3E53] hover:bg-orange-800 focus:ring-4 focus:ring-orange-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 focus:outline-none shadow-md"
+            >
+              Get started
+            </Link>
+          </div>
+          <div
+            className="hidden justify-between items-center w-full lg:flex lg:w-auto lg:order-1"
+            id="mobile-menu-2"
+          >
+            <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
+              <li>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
+                                        ${
+                                          isActive
+                                            ? `text-orange-700`
+                                            : `text-gray-700`
+                                        }
+                                        lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Home
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
+                                        ${
+                                          isActive
+                                            ? `text-orange-700`
+                                            : `text-gray-700`
+                                        }
+                                        lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  About
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
+                                        ${
+                                          isActive
+                                            ? `text-orange-700`
+                                            : `text-gray-700`
+                                        }
+                                        lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Contact
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/github"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
+                                        ${
+                                          isActive
+                                            ? `text-orange-700`
+                                            : `text-gray-700`
+                                        }
+                                        lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  Github
+                </NavLink>
+              </li>
+
+              <li>
+                <NavLink
+                  to="/user"
+                  className={({ isActive }) =>
+                    `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100
+                                        ${
+                                          isActive
+                                            ? `text-orange-700`
+                                            : `text-gray-700`
+                                        }
+                                        lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                  }
+                >
+                  User
+                </NavLink>
+              </li>
+            </ul>
           </div>
         </div>
-      </div>
-    </div>
-  </div>
-</header>
-
-    
-
-
-
-  )
+      </nav>
+    </header>
+  );
 }
-
-export default Header
